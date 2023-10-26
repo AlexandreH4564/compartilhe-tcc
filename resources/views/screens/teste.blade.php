ac24d262a56507b1d62de5338caefa8123ae77e7
alexandreh4564@gmail.com <you@example.com>
2023-10-26 09:46:32 -0300

    teste

--- resources/views/screens/teste.blade.php
+++ resources/views/screens/teste.blade.php
@@ -46,6 +46,7 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      {{-- teste --}}
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
